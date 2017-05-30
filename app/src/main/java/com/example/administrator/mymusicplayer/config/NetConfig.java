7dabf176919abb1cfd0aa5c7c00c831adde9ca86
xiaonizi <xiaonizi919@163.com>
2017-05-30 16:18:19 +0800

    添加

--- app/src/main/java/com/example/administrator/mymusicplayer/config/NetConfig.java
+++ app/src/main/java/com/example/administrator/mymusicplayer/config/NetConfig.java
@@ -8,8 +8,8 @@ public interface NetConfig {
     String BASE_VIDEO_PATH="https://cms.loongscity.com/cityparlor-web/mobile/cityparlor/vod/video/list?";//视频接口
     String VIDEO_Type="11f521fe-6a8d-4acc-b021-f997c3ec661b";//视频接口
 
-    String LIANG_LIANG = "http://172.16.47.206:8080/Music/liangliang.mp3";//凉凉  张碧晨&杨宗纬
-    String PIAO_YANG_GUOHAI = "http://172.16.47.206:8080/Music/pyghlkn.mp3";//漂洋过海来看你  朱亚文&王丽坤
-    String GANG_HAO_YU_JIAN_NI = "http://172.16.47.206:8080/Music/ghyjn.mp3";//刚好遇见你  李玉刚
-    String XIN_YANG = "http://172.16.47.206:8080/Music/xinyang.mp3"; //信仰  张信哲
+    String LIANG_LIANG = "http://192.168.1.104:8080/Music/liangliang.mp3";//凉凉  张碧晨&杨宗纬
+    String PIAO_YANG_GUOHAI = "http://192.168.1.104:8080/Music/pyghlkn.mp3";//漂洋过海来看你  朱亚文&王丽坤
+    String GANG_HAO_YU_JIAN_NI = "http://192.168.1.104:8080/Music/ghyjn.mp3";//刚好遇见你  李玉刚
+    String XIN_YANG = "http://192.168.1.104:8080/Music/xinyang.mp3"; //信仰  张信哲
 }
